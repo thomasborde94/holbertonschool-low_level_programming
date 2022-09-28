3c041e94c4dd7b23be0d449f9b6e3b2b265328e5
Thomas Borde <thomas.borde94@hotmail.fr>
2022-09-28 00:48:16 -0700

    changed script 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints a string
+ * Return: Always 0
+ */
+
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,");
+  return (0);
+}
