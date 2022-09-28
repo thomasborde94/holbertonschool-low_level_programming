a984fc1350f358ffef5f3e5a3586948ca4351f79
Thomas Borde <thomas.borde94@hotmail.fr>
2022-09-28 01:24:15 -0700

    added script 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main -Entry point
+ * Description: prints the size of various types on the computer
+ * Return: always 0
+ */
+
+int main(void)
+{
+  char charType;
+  int intType;
+  long int longintType;
+  long long int longlongType;
+  float floatType;
+
+  printf("Size of char: %zu byte(s)\n", sizeof(charType));
+  printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+  printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+  printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+  printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+  return (0);
+}
