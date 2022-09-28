42272a9b47137dd67adfa7d25205fbfef6f8107f
Thomas Borde <thomas.borde94@hotmail.fr>
2022-09-28 01:32:04 -0700

    changed script 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ long int longintType;
 long long int longlongType;
 float floatType;
 
-printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
