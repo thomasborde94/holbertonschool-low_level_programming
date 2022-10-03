66fdbb8ea7726e639658b1c3cb19ea4be9cea875
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-03 08:35:58 -0700

    changed script 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		printf("+");
 		return (1);
 	}
 	if (n < 0)
 	{
-		printf("-1, ");
+		printf("-1");
 		return (-1);
 	}
 	else
 	{
-		printf("0, ");
+		printf("0");
 		return (0);
 	}
 }
