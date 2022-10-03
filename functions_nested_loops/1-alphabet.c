cbf23acb5c44d07deac2768fac76dbe7dd5df223
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-03 04:14:18 -0700

    changed script 01

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code
- * Description: check the code
+ * print_alphabet - print alphabet
+ * Description: prints alphabet lowercase
  * Return: always 0
  */
 
