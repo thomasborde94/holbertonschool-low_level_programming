26b2904a8a30d7c11cc5e74396246b9b7fc06c44
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-03 00:22:05 -0700

    added script 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the alphabet
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != e || ch != q)
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
