ef5066e3fe37beff660cb8e3fbf964cd0e8c3f01
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-02 23:45:40 -0700

    changed 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, y);
 	}
-	if (y < 5)
+	if (y < 6 && !=0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 	}
