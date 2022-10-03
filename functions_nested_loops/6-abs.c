ded3ef924037aebf4596ebb87e93e957f74679a3
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-03 08:54:43 -0700

    added script 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,27 +1,20 @@
 #include "main.h"
 
 /**
- * sign - a
- * @c: integer
+ * _abs - a
+ * @n: integer
  * Description: checks input is negative or positive of nul
  * Return: 1 if c is positive, -1 if negative, 0 otherwise
  */
 
-int sign(int n)
+int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-		printf("+\n");
-		return (1);
-	}
-	if (n == 0)
-	{
-		printf("0\n");
-		return (0);
+		printf("%d", n);
 	}
 	if (n < 0)
 	{
-		printf("-1\n");
-		return (-1);
+		printf("%d", n * -1);
 	}
 }
