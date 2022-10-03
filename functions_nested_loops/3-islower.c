758924959a963788e2a2d0821179be1755d743ad
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-03 06:20:43 -0700

    changed 3 for betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * islower - a
- * Description: checks for lowercase character
+ * _islower - a
+ * @c: integer
+ * Description: checks for lowercase characters
  * Return: 1 if c is lowercase, 0 otherwise
  */
 
@@ -13,4 +14,3 @@ int _islower(int c)
 	else
 		return (0);
 }
-		       
