3df9fa507815880a0dc748cc15405a366eeb4369
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-02 23:10:26 -0700

    changed 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
- /**
- * main - Entry point
-
- * Description: prints whether n is positive, negative, or 0
-
- * Return: always 0
- */
+/**
+* main - Entry point
+* Description: prints whether n is positive, negative, or 0
+* Return: always 0
+*/
 
 int main(void)
 {
