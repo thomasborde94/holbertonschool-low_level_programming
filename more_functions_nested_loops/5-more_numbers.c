40c7de226593528c9a8cb3fde63ad76ee1c2411e
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-04 12:08:25 -0700

    added script 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ * Description: prints 10 times the numbers, from 0 to 14, + a new line
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+			{
+				_putchar('0' + n);
+			}
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
+	}
+}
