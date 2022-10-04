682325f04d3a5b99fada61cf5bcc1320fd0cee2d
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-04 10:56:58 -0700

    added script 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if digit
+ * @c: integer to check
+ * Description: checks if input is a digit
+ * Return: 1 if digit, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
