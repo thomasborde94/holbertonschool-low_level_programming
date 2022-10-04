43a98c1adb1e5b3cf949c6cacb00c18a2ae40a53
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-04 11:19:40 -0700

    added script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies
+ * @a: first input to multiply
+ * @b: second input to multiply
+ * Description: multiplies a and b int
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
