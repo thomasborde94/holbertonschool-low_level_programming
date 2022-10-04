2132cdc422f992ccf0ff75db81316638b2f80027
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-04 11:47:26 -0700

    changed 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
+ * print_most_numbers - prints numbers
  * Descriptionn: prints numbers from 0 to 9, except 2 and 4
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
