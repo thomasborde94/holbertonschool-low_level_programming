193e609f829f6c6951229ba4bcfd83ba8a2a7c32
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-05 04:09:05 -0700

    changed 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ void fizz_buzz(void)
 
 	for (i = 1; i <= 100; i++)
 	      {
-		      if (i % 3 == 0 && i % 5 == 0)
+		      if ((i % 3 == 0) && (i % 5 == 0))
 		      {
 			      printf("FizzBuzz ");
 		      }
