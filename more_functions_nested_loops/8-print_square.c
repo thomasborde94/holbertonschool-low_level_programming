4f6c04a3d51a4da1b398f5c342455c6693c3184c
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-05 00:19:42 -0700

    added 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * Description: prints a square with # of size size
+ * @size: size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k < i; k++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
