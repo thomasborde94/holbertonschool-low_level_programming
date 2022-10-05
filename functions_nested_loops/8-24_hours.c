d9810252619db6589b6f804917b665927d0e69e8
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-05 04:18:50 -0700

    changed 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,10 +10,17 @@ void jack_bauer(void)
 {
 	int m, h;
 
-
-
-
-
-	_putchar('\n');
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
 }	
 		
