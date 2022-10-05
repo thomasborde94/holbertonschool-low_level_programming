582f470f3b9fb1e0ce7c0ec60283418033f68157
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-05 03:51:57 -0700

    changed 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - prints numbers from 1 to 100
+ * Description: prints from 1 to 100, where 3 multiples
+ * are replaced by Buzz, 5 by Fizz, and both by FizzBuzz
+ * Return: void
+ */
+
+void fizz_buzz(void)
+{
+}
