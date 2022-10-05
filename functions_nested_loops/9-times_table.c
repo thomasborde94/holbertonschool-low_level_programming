4fc966126da2c32ee97fcd237f568623291c09e3
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-05 04:43:34 -0700

    changed 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,12 +8,13 @@
 
 void times_tables(void)
 {
-	int d, i, prod = i * d;
+	int d, i, prod;
 
 	for (d = 0; d < 10; d++)
 	{
 		for (i = 0; i < 10; i++)
 		{
+			prod = d * i;
 			_putchar('0' + prod);
 			_putchar(',');
 			_putchar(' ');
