3e80df831ff75233ed14f842ecbdb19fa5242b39
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-06 00:52:36 -0700

    added script 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - adds two int
+ * Description: adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: the result of the two ints
+ */
+
+int main(int a, int b)
+{
+	return (a + b);
+}
