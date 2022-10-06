100c552bcd52f5fdf5ad63b0b35f4f05f4421ddf
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-06 03:36:50 -0700

    changed 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 			printf("%d, ", n++);
 	}
 	else
 	{
-		while (n >= 98)
+		while (n > 98)
 			printf("%d, ", n--);
 	}
 	printf("\n");		
