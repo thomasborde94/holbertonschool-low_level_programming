4200a8c7dee19fa41b537192a9126ed4c3cf05aa
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 02:24:41 -0700

    changed 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -14,9 +14,9 @@ void print_rev(char *s)
 	while (s[length] != '\0')
 		length++;
 
-	for (; length != 0; s[length--])
+	for (; length != 0; length--)
 	{
-		_putchar(*s[length]);
+		_putchar(s[length]);
 	}
 	_putchar('\n');
 }
