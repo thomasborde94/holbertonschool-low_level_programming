a6c67063dde60b27c631e65ba3320d77be5701ce
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 01:53:37 -0700

    added script 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * Description: ptins a string, followed by a newline
+ * to stdout
+ * @str: string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		*str++;
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
-#include <stdio.h>
+0;10;1c0;10;1c#include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
