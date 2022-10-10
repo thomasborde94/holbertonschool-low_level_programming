f6536e245e54a6716a1454f9ee77757f8d7a2cee
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 04:13:20 -0700

    changed 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-#include "main.h"
+0;10;1c0;10;1c#include "main.h"
 
 /**
  * rev_string - reverses a string
@@ -18,7 +18,7 @@ void rev_string(char *s)
 		length++;
 	}
 
-	for (i = length - 1; i >= length / 2; i--)
+	for (i = length - 1; i >= length / 2 - 1; i--)
 	{
 		newI = s[i];
 		s[i] = s[length - 1 - i];
