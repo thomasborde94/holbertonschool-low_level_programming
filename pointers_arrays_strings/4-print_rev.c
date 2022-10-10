f98e570141f985fb58d48b7111e80c291d16930d
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 02:18:18 -0700

    added script 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * Description: prints a string in reverse, then newline
+ * @s: string to print in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (; length != 0; length--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
