9debbbbe16028aef10077f764faed7776b0b8b82
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 05:30:48 -0700

    changed 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n -1)
+		if (i != n - 1)
 		{
-			_putchar(a[i]);
+			printf("%d", a[i]);
 			_putchar(',');
 			_putchar(' ');
 		}
