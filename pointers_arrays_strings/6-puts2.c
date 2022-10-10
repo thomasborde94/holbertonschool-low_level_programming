fc3b451ffc7c98b11cf0dab6a37cf634d18b11fa
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 04:28:46 -0700

    added 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * Description: prints every other character of a string
+ * @str: string to use
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '0')
+		length++;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	}
+	_putchar('\n');
+}
