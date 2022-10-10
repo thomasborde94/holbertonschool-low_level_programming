700c577d9af04716413795e6ddfca2d76d401ab3
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-10 04:34:53 -0700

    changed 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -15,10 +15,8 @@ void puts2(char *str)
 	while (str[length] != '0')
 		length++;
 
-	for (i = 0; i < length; i++)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
