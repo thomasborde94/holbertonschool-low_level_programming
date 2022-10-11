5c1a8437ef30e018195b3f5367d61f31c71789b2
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-11 00:53:09 -0700

    changed 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	while (dest[i])
+	while (dest[i] != '\0')
 		i++;
 
-	while (src[j])
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
