3f22bebedfa0caf7852ae6ee287dda858497eb82
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-11 02:48:36 -0700

    corrected 3

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int _strcmp(char *, char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[] = "Holberton";
+	char s2[] = "Holbeerton";
+
+	printf("%d\n", _strcmp(s1, s2));
+	return (0);
+}
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j = 0;
-
-	for (i = 0; s1[i] == s2[i] && s1[i] && s2[i]; i++)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] < s2[i])
-			j--;
-
-		if (s1[i] > s2[i])
-			j++;
-
-		if (s1[i] == s2[i])
-			continue;
+		s1++;
+		s2++;
 	}
 
-	return (j);
+	return (*s1 - *s2);
 }
