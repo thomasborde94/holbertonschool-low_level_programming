28fb7e594ea90154ee86cf6196d684bac8a668f9
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-11 02:04:10 -0700

    added script 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * Description: compares two string
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: comparaison of the strings as int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, j = 0;
+
+	for (i = 0, s1[i] == s2[i] && s1[i] == '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+			j--;
+
+		if (s1[i] > s2[i])
+			j++;
+
+		if (s1[i] == s2[i])
+			continue;
+	}
+
+	return (j);
+}
