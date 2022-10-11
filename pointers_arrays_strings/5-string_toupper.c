9ffa72d85214f68ec78b177e8fe0ea6d2280fb11
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-11 08:43:35 -0700

    added 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * Description: Changes all lowercase letters of a string
+ * to uppercase
+ * @str: the string to change
+ * Return: a pointer to the changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	while (str[a])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			p[i] -= 32;
+		i++;
+	}
+
+	return (str);
+}
