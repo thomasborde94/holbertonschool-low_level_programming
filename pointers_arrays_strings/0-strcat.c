6faa1f43006d05d7c806d123734c1c10582e3a60
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-11 00:04:08 -0700

    added 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - appends two strings
+ * Description: appends src string to dest string
+ * @dest: string to append to
+ * @src: string to append from
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int length = 0;
+
+	while (dest[length])
+		length++;
+
+	for (i = 0; src[i]; i++)
+	{
+		
+	}
+}
