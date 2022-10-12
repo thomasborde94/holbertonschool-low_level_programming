11b869a3029a1698a8f1c0ba8ac687316b52c8fb
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-12 03:17:46 -0700

    added script 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,4 @@ char *cap_string(char *str);
 char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
