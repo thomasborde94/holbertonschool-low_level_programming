f254ecd2b9ad4bb6456351c15c8dbeb31fde6088
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-12 05:44:33 -0700

    added script 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set
+ * of bytes
+ * Description: scans string s and finds the first character in
+ * the string s that matches any character specified in string accept
+ * @s: string to scan
+ * @accept: string containing the characters to match
+ * Return: a pointer to the character, or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if (s == NULL || accept == NULL)
+		return NULL;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+}
