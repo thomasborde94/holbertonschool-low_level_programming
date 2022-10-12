9184fa8d5f2c0886cf2ad5f5aa4fd8f05a381279
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-12 02:05:10 -0700

    added 0 memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - changes value of bytes
+ * Description: copies the value of b into each
+ * of the first n characters of the object pointed to
+ * by s
+ * @s: pointer to the destination object
+ * @b: value to be filled
+ * @n: number of bytes to be filled
+ * Return: pointer with new values
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (*s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,3 +19,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *_memset(char *s, char b, unsigned int n);
