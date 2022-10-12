48d52f92377fc21a83017e5c8a28f9abc6ba3d73
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-12 01:09:20 -0700

    changed script 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -16,12 +16,12 @@ char *leet(char *str)
 	char toReplace[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char code[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i]; i++)
 	{
 		for (j = 0; j <= 10; j++)
 		{
-			if (str[i] == toReplace[i])
-				str[i] = code[i];
+			if (str[i] == toReplace[j])
+				str[i] = code[j];
 		}
 	}
 	return (str);
