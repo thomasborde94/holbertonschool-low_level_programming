4029caaf024c03d437a4c1dab3796c21622d235d
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-12 04:53:06 -0700

    added script 3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -20,7 +20,5 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	if (c == '\0')
-		s = '\0';
 	return (NULL);
 }
---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspn - returns length of a string made only
+ * of characters contained in another string
+ * Description: Returns length of string s written
+ * only with accept characters
+ * @s: string to check
+ * @accept: characters accepted
+ * Return: numbers of characters in s which consist
+ * only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; *accept; i++)
+			count++;
+		s++;
+	}
+
+	return (count)
+}
