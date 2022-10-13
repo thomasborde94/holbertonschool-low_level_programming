29a86f555504ba10d613ab52aad70c0f6a3a9cd1
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-13 01:29:07 -0700

    added strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - finds first occurrence of a substring in a string
+ * Description: Finds the first occurence of the substring
+ * needle in the string haystack
+ * @needle: substring
+ * @haystack: where we search for the occurence
+ * Return: pointer to the first occurence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)// tant qu'on arrive pas au NULL terminator
+	{
+		char *begin = haystack;// crée un pointer qui a comme valeur l'adresse de haystack
+		char *pattern = needle;// crée un pointer qui a comme valeur l'adresse de needle
+
+		while (*haystack && *pattern && *haystack == *pattern)//Si un character match un character de needle, on check le reste de la string
+		{
+			haystack++;
+			pattern++;
+		}
+
+		if (!*pattern)// 
+			return begin;
+
+		haystack = begin + 1;
+	}
+	return (NULL);
+}
+
