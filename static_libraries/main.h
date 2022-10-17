a28a3537e174dc19294777a2285f72122d9fc2ee
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-17 07:05:49 -0700

    added include guard

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-
+#IFNDEF MAIN
+#DEFINE MAIN
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#ENDIF
