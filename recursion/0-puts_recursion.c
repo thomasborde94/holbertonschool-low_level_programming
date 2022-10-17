d30b9e37c96dd3022b7de6eb08ea61ef17019093
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-17 00:01:51 -0700

    added 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * Description : prints a string followed by a newline
+ * @s: string to print
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	while (s++)
+		_putchar("*s");
+}
