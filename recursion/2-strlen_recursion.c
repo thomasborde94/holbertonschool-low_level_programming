f05dc475a12b44a163eb80edd1b87e47709dca42
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-17 00:43:52 -0700

    added script 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion(char *s) - function that return length
+ * of a string
+ * Description: returns the length of a string
+ * @s: string to return the length of
+ * Return: int length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (i);
+
+	else
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+}
