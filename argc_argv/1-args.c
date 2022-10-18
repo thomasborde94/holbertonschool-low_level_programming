4812676f0cbecf4914559cdefb11ee010ad5ee4f
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-18 00:17:15 -0700

    added script 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into
+ * the program
+ * Description: prints the number of argmuents passed into
+ * the program
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
