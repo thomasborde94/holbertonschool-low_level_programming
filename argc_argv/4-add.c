373c040bd388f1134e2c2420b5d8a6fc9d7cce09
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-18 01:07:54 -0700

    changed 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -16,23 +16,17 @@ int main(int argc, char *argv[])
 	int symbol;
 	int sum;
 
-	if (argc == 1)
-		printf("0\n");
-
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			for (symbol = 0; argv[i][symbol]; symbol++)
-			{
-				if (argv[i][symbol] < 48 || argv[i][symbol] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			sum += atoi(argv[i]);
-		}
+       	for (i = 1; i < argc; i++)
+       	{
+       		for (symbol = 0; argv[i][symbol]; symbol++)
+       		{
+       			if (argv[i][symbol] < 48 || argv[i][symbol] > 57)
+       			{
+       				printf("Error\n");
+       				return (1);
+       			}
+       		}
+       		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
