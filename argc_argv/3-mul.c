39f904411fc4f352c0e14f471047fab5031a7ed0
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-18 00:33:36 -0700

    added script 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints result of multiplication
+ * Description: prints result of multiplication
+ * @argc: argc
+ * @argv: argv
+ * Return: 0, 1 if not two arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int prod;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; i < argc; i++)
+		sum += atoi(argv[i]);
+
+	return (0);
+}
