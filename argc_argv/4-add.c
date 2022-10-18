97663c2e91b2f2518570cc7bd014b7e297d3f55c
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-18 01:01:26 -0700

    added script 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ * Description: adds positive numbers
+ * @argc: argc
+ * @argv: argv
+ * Return: 1 if number contains symbols that are not digits
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int symbol;
+	int sum;
+
+	if (argc < 3)
+		printf("0\n");
+
+	for (i = 1; i < argc; i++)
+	{
+		for (symbol = 0; argv[i][symbol]; symbol++)
+		{
+			if (argv[i][symbol] < 48 || argv[i][symbol] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
