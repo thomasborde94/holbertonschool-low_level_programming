67421bdc742b207f001e32eceae921d3520f39b3
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-17 23:51:29 -0700

    added script 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - write its name
+ * Description: write its name
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[1]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Exercices about argc and argv[]
