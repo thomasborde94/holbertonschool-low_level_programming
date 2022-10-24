556f19bb0c31eca5e8a808fd588015a2a7136c12
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-24 00:41:35 -0700

    changed 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,10 +2,10 @@
 
 /**
  * create_array - creates an array of char
- * Description: creates an array of char
+ * Description: creates an array of char initialized with c
  * @size: size of the array
  * @c: character to initialize the array
- * Return : pointer to the array
+ * Return: pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(sizeof(char) * size);
 
-	if (array = NULL)
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
