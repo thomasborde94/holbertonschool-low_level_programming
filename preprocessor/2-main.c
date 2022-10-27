e141c8e7fb6fd531f43b81bce39e130a94f08e51
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-27 01:36:02 -0700

    added script 0

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints file it is compiled from
+ * Description: prints file it compiled from
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
