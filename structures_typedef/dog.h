47c877a6d3eda2f94d9ee174615f965b1255062e
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 02:20:29 -0700

    added 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,21 @@ typedef struct dog
 	char *owner;
 } dog;
 
+/**
+ * struct dog - defines new name for dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: defines new name for dog struct
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
