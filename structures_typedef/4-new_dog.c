a230826a9dc1f3389e8a840e65773cbde4b76602
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 03:43:54 -0700

    changed 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,6 +23,13 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
+/**
+ * _strlen - returns length of str
+ * Description: returns length of str
+ * @str: string to get length from
+ * Return: length of str
+ */
+
 int _strlen(char *str)
 {
 	int length = 0;
@@ -46,14 +53,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dawg;
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	dawg = malloc(sizeof(dog_t));
-		      
+	if (dawg == NULL)
+		return (NULL);
+
+	dawg->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dawg->name == NULL)
+	{
+		free(dawg);
+		return (NULL);
+	}
+	dawg->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dawg->owner == NULL)
+	{
+		free(dawg);
+		return (NULL);
+	}
+
 	dawg->name = _strcpy(dawg->name, name);
 	dawg->age = age;
 	dawg->owner = _strcpy(dawg->owner, owner);
 
-	if (dawg == NULL)
-		return (NULL);
-	else
-		return (dawg);
+	return (dawg);
 }
