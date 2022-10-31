f8d7d4816742dcf123f68dd6ce209207b75d66f5
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 05:22:25 -0700

    added 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * Description: frees dogs
+ * @d: dog to free
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
