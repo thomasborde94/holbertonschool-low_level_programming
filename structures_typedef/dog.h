c47a85f80ceac2577ce78a304a044efae3a11a76
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 02:23:09 -0700

    changed dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,27 +9,18 @@
  * Description: defines dog struct
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 /**
- * struct dog - defines new name for dog struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Description: defines new name for dog struct
+ * dog_t - typedef for struct dog
  */
 
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+typedef struct dog dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
