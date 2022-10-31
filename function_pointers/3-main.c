d1ac038df4e6ae7bb213c48315cf1de19dd93cf2
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 10:14:17 -0700

    changed 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,27 +11,30 @@
 
 int main(int argc,char *argv[])
 {
-	int result;
-	char *operator;
+	int result, num1, num2;
+	char *op;
 
-	operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (*operator != "+" || *operator != "-" || *operator != '*' || *operator != '/' || *operator != '%')
+	if (*op != "+" || *op != "-" || *op != '*' || *op != '/' || *op != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if(*operator == '/' && *argv[3] == "0" || *operator == '%' && *argv[3] == "0")
+	if(*op == '/' && num2 == "0" || *op == '%' && num2 == "0")
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	result = get_op_func(operator)(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
