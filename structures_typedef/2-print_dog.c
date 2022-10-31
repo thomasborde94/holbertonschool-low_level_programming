4cf51d6719709dd7f96a75bddaba90100eeaa514
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 02:14:13 -0700

    changed 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,19 +12,19 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d.name == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)");
 		else
-			printf("Name: %s\n", d.name);
+			printf("Name: %s\n", d->name);
 
-		if (d.age == NULL)
+		if (d->age == NULL)
 			printf("Age: (nil)");
 		else
-			printf("Age: %f\n", d.age);
+			printf("Age: %f\n", d->age);
 
-		if (d.owner == NULL)
+		if (d->owner == NULL)
 			printf("Owner: (nil)");
 		else
-			printf("Owner: %s", d.owner);
+			printf("Owner: %s", d->owner);
 	}
 }
