120a3b249858c0891e49d2133a00406ab6ce3dad
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 01:39:09 -0700

    changed 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes a struct dog variable
@@ -12,5 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	dog d = {*name, age, *owner);
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
