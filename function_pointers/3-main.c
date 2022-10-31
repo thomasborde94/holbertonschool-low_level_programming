d38149e6a38114be1fed593cf1980efa3b1e5fe8
Thomas Borde <thomas.borde94@hotmail.fr>
2022-10-31 09:53:36 -0700

    changed 3.main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,7 +3,8 @@
 /**
  * main - main
  * Description: main function
- * Return: If the number of args is wrong, exit status 98
+ * Return: Always 0, but
+ * If the number of args is wrong, exit status 98
  * If the operator is not correct, exit status 99
  * If trying to divide or % by 0, exit status 100
  */
@@ -11,22 +12,26 @@
 int main(int argc,char *argv[])
 {
 	int result;
+	char *operator;
+
+	operator = argv[2];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != "+" || argv[2] != "-" || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+	if (*operator != "+" || *operator != "-" || *operator != '*' || *operator != '/' || *operator != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if(argv[2] == '/' && argv[3] == "0" || argv[2] == '%' && argv[3] == "0")
+	if(*operator == '/' && *argv[3] == "0" || *operator == '%' && *argv[3] == "0")
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-	return (result);
+	printf("%d\n", result);
+	return (0);
 }
