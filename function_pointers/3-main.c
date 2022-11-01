8e05e8e9956d745bbfc805766c9b707e9d30ffb2
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-01 01:50:10 -0700

    changed 3 main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,15 +14,16 @@ int main(int argc,char *argv[])
 	int result, num1, num2;
 	char *op;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op = argv[2];
-	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
