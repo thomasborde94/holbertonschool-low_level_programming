edce4992a3f31be12d57b53cae2e8b341b7da792
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-01 02:07:11 -0700

    changed 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -20,10 +20,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		if (*(ops[i].op) == *s)
-		    break;
+			break;
 		i++;
 	}
 
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,13 +3,15 @@
 /**
  * main - main
  * Description: main function
+ * @argv: argv
+ * @argc: argc
  * Return: Always 0, but
  * If the number of args is wrong, exit status 98
  * If the operator is not correct, exit status 99
  * If trying to divide or % by 0, exit status 100
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 	char *op;
@@ -23,13 +25,13 @@ int main(int argc,char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 	op = argv[2];
-	
-	if (get_op_func(argv[2]) == NULL)
+
+	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
