4e2f8fad3d333caa67d8c42b103121645e0d060b
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-02 02:30:46 -0700

    changed 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -20,8 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
-		else
-			printf("\n");
 	}
+	printf("\n");
 	va_end(args);
 }
