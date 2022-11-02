4a636e3210f92acec482049b78956a9ef0c83eb0
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-02 02:17:43 -0700

    changed 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers
+ * Description: prints numbers, followed by newline
+ * @separator: string to print between numbers
+ * @n: number of ints to print
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+	}
+	va_end(args);
+}
