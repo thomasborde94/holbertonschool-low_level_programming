9bfae13868f2c26b993d61100c879473e4bdbd13
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-02 03:34:21 -0700

    changed 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,6 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list args;
 
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
