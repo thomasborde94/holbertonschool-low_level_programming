07c6827f38ed2a29eb67c2bdc9079c6fc824fc1d
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-02 02:56:03 -0700

    changed 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (!str)
 			printf("(nil)");
 		else
-			printf("%s", str));
+			printf("%s", str);
 
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
