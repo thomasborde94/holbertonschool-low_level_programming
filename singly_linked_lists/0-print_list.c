ef3434384effcffa4a12b956acd2657a4a2f6a8e
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-14 01:05:10 -0800

    added script 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+size_t print_list(const list_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+
+		else
+			printf("[%d] %s", h->len, h->str);
+
+		nodeCount++;
+		h = h->next;
+	}
+	return (nodeCount);
+}
