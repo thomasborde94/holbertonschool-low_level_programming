1db2ea67ab67e8b35a3da365dc31cf1c6b1e7847
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-14 01:08:27 -0800

    created lists.h

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "lists.h"
 
 size_t print_list(const list_t *h)
 {
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,9 @@
+#ifndef __LISTS__
+#define __LISTS__
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+#endif
