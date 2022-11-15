3011f3ffe5fdec514dbb3d9f8e0cfebe79e1ca06
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-15 01:07:23 -0800

    added script 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * Description: frees a list
+ * @head: pointer to list to free
+ * Return: void
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+		free(temp);
+	}
+	free(head);
+}
