a12d440c87c59513990ee115ca818155260a9aff
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-15 05:34:04 -0800

    changed 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -10,13 +10,19 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tempNode, *prevNode;
+	listint_t *tempNode, *nextNode;
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (-1);
 	tempNode = *head;
 
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(tempNode);
+		return (1);
+	}
 	while (i < index - 1)
 	{
 		if (tempNode == NULL || tempNode->next == NULL)
@@ -25,14 +31,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(tempNode);
-		return (1);
-	}
-	prevNode = tempNode->next;
-	tempNode->next = prevNode->next;
-	free(prevNode);
+	nextNode = tempNode->next;
+	tempNode->next = nextNode->next;
+	free(nextNode);
 	return (1);
 }
