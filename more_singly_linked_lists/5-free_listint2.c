39dc1c2a2accf80b0da98c9fc1457cd7e27766b9
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-15 01:22:57 -0800

    added script 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * Description: frees a list and sets the head to null
+ * @head: pointer to head of the list to free
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	head = NULL;
+}
