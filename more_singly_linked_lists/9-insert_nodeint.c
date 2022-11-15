fd6534ebbdfd6a1b6b52045cf686a1fc2a03859f
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-15 04:31:44 -0800

    changed 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -16,9 +16,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
 	tempNode = *head;
 
-	while (i < idx - 1)
+	if (idx == 0)
+	{
+		newNode->next = tempNode;
+		*head = newNode;
+		return (newNode);
+	}
+	while (i < (idx - 1))
 	{
 		if (tempNode == NULL || tempNode->next == NULL)
 			return (NULL);
@@ -26,19 +37,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
-	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-	newNode->n = n;
-	if (idx == 0)
-	{
-		newNode->next = tempNode;
-		*head = newNode;
-		return (newNode);
-	}
 	newNode->next = tempNode->next;
 	tempNode->next = newNode;
 	return (newNode);
