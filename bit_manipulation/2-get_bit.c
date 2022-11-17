5b59c8cee56576581cee1c4c668c5214d5755dea
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-17 00:45:55 -0800

    changed 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,6 +12,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int result;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
 	result = (n >> index) & 1;
 	return (result);
 }
