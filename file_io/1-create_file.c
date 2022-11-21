e6181746315704e60932756902245ae26a74e7b2
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-21 08:11:04 -0800

    added 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * Description: creates a file
+ * @filename: file to create
+ * @text_content: text to write in filename
+ * @return: 1 on success, else -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[length])
+			length++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	w = write(fd, text_content, length);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
