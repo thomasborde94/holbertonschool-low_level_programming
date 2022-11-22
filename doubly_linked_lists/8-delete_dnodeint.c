12d64c41c0ae11f4722444b99b1a53924cc452ee
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-22 01:15:53 -0800

    added script 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at given position
+ * Description: deletes a node at given position
+ * @head: list to go through
+ * @index: given position
+ * Return: 1 if succeeded, else -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tempNode, *nextNode;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (-1);
+	tempNode = *head;
+	if (tempNode == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		free(tempNode);
+		return (1);
+	}
+	while (i < (index - 1))
+	{
+		if (tempNode == NULL || tempNode->next == NULL)
+			return (-1);
+		tempNode = tempNode->next;
+		i++;
+	}
+	nextNode = tempNode->next;
+	tempNode->next = nextNode->next;
+	tempNode = tempNode->next->prev;
+	free(nextNode);
+	return (1);
+}
