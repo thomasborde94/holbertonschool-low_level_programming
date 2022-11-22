44f0f648fef26f169b212a18d5e8c8d754ca8548
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-22 01:17:44 -0800

    changed 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		(*head)->prev = NULL;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(tempNode);
 		return (1);
 	}
