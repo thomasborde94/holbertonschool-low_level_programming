28f40e0c357381ec625a22f5d7cfcfb021a76fa7
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-22 00:05:41 -0800

    changed 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->n = n;
 	newNode->prev = NULL;
 	newNode->next = *head;
-	(*head)->prev = newNode;
+	if (*head != NULL)
+		(*head)->prev = newNode;
 	*head = newNode;
 	return (newNode);
 }
