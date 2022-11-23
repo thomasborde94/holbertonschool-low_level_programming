5451af2801fabbc1ba49c8957dfc427a96a5a8c8
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-23 01:36:56 -0800

    added 1

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+#include <ctype.h>
+
+/**
+ * hash_djb2 - hash function implementing the djb2 algo
+ * Description: hash function implementing the djb2 algo
+ * @str: datat to send to hash table
+ * Return: hash code
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		if (isupper(c))
+		{
+			c = c + 32;
+		}
+
+		hash = ((hash << 5) + hash) + c;
+	}
+
+	return (hash);
+}
