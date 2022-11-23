ce5e3f2d680577ce5af5cb239785329f6326855e
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-23 00:44:10 -0800

    added 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates an hash table
+ * Description: creates an hash table of size size
+ * @size: size of the hash table
+ * Return: pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	while (i < size)
+	{
+		ht->array[i] == NULL;
+		i++;
+	}
+
+	return (ht);
+}
