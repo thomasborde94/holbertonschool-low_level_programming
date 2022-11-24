36f984ee86ad3aae5e37440b87adbd7bbf0fafdc
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-24 00:18:11 -0800

    changed 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	valueCopy = strdup(value);
-	slot = key_index(key, ht->size);
+	slot = key_index((const unsigned char *)key, ht->size);
 	item = ht->array[slot];
 	if (item == NULL)
 	{
