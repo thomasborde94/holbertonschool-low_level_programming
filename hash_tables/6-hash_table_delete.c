307cf005d6ee11fe3b312dcebd13608b26cc6094
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-24 23:46:21 -0800

    changed 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -20,11 +20,11 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node;
-				node = node->next;
-				free(tmp->key);
-				free(tmp->value);
-				free(tmp);
+				tmp = node->next;
+				free(node->value);
+				free(node->key);
+				free(node);
+				node = tmp;
 			}
 		}
 		i++;
