c8ef4c54321d66a68b9bc0bc546aa3a141b69f2d
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-25 01:18:10 -0800

    changed 3 to try solving valgrind issues

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,9 +13,24 @@
 hash_node_t *create_item(const char *key, const char *value)
 {
 	hash_node_t *item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+	{
+		free(item);
+		return (0);
+	}
 
 	item->key = malloc(strlen(key) + 1);
+	if (item->key == NULL)
+	{
+		free(item);
+		return (0);
+	}
 	item->value = malloc(strlen(value) + 1);
+	if (item->value == NULL)
+	{
+		free(item->value);
+		return (0);
+	}
 
 	strcpy(item->key, key);
 	strcpy(item->value, value);
