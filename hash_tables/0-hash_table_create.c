9bec5684d1868df11e2b225a9d86ee1a8486b0fa
Thomas Borde <thomas.borde94@hotmail.fr>
2022-11-25 00:01:51 -0800

    changed 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = malloc(sizeof(hash_table_t));
-	unsigned long int i;
+	unsigned long int i = 0;
 
 	if (ht == NULL)
 	{
